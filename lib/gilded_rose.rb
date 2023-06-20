7d26df1756c1f73511e776028d838f0285067bc9
senseiseo <1919mur@gmail.com>
2023-06-20 16:27:22 +0300

    рефакторинг

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+# Класс GildedRose отвечает за обновление качества и срока годности товаров в магазине Gilded Rose.
+# Он содержит методы для обновления качества различных типов товаров, таких как обычные товары,
+# товары Aged Brie, Backstage passes и Sulfuras. Класс также предоставляет метод `update_quality`,
+# который обновляет качество всех товаров в списке `items`.
 class GildedRose
   attr_reader :items
 
@@ -12,57 +18,56 @@ class GildedRose
 
   private
 
-  AGED_BRIE = "Aged Brie"
-  BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
-  SULFURAS = "Sulfuras, Hand of Ragnaros"
+  AGED_BRIE = 'Aged Brie'
+  BACKSTAGE_PASSES = 'Backstage passes to a TAFKAL80ETC concert'
+  SULFURAS = 'Sulfuras, Hand of Ragnaros'
+  CONJURED = 'Conjured'
   private_constant :AGED_BRIE, :BACKSTAGE_PASSES, :SULFURAS
 
-  def def update_item(item)
+  def update_item(item)
+    item.sell_in -= 1 unless item.name == SULFURAS
+
     case item.name
     when AGED_BRIE
       update_aged_brie(item)
     when BACKSTAGE_PASSES
       update_backstage_passes(item)
+    when SULFURAS
+      update_sulfaras
+    when /#{Regexp.escape(CONJURED)}/i
+      update_conjured(item)
     else
       update_ordinary_item(item)
     end
-
-    item.sell_in -= 1 unless item.name == SULFURAS
-    update_expired_item(item) if item.sell_in < 0
   end
 
   def update_aged_brie(item)
     increase_quality(item)
-    increase_quality(item) if item.sell_in < 0
+    increase_quality(item) if item.sell_in.negative?
   end
 
   def update_backstage_passes(item)
-    if item.sell_in < 0
+    if item.sell_in.negative?
       item.quality = 0
-    elsif item.sell_in < 6
+    elsif item.sell_in < 5
       increase_quality(item, 3)
-    elsif item.sell_in < 11
+    elsif item.sell_in < 10
       increase_quality(item, 2)
     else
       increase_quality(item)
     end
   end
 
-  def update_ordinary_item(item)
-    decrease_quality(item)
-    decrease_quality(item) if item.sell_in < 0
+  def update_sulfaras; end
+
+  def update_conjured(item)
+    decrease_quality(item, 2)
+    decrease_quality(item, 2) if item.sell_in.negative?
   end
 
-  def update_expired_item(item)
-    case item.name
-    when AGED_BRIE
-      increase_quality(item)
-    when BACKSTAGE_PASSES
-      item.quality = 0
-    else
-      decrease_quality(item)
-      decrease_quality(item) if item.sell_in < 0
-    end
+  def update_ordinary_item(item)
+    decrease_quality(item)
+    decrease_quality(item) if item.sell_in.negative?
   end
 
   def increase_quality(item, amount = 1)
@@ -74,6 +79,8 @@ class GildedRose
   end
 end
 
+# Класс Item представляет собой общий товар в инвентаре магазина Gilded Rose.
+# Он содержит атрибуты: название, срок годности и качество.
 class Item
   attr_accessor :name, :sell_in, :quality
 
